f7a3acd455f8d130b590cbd5c3de517ceb0f5405
Yang Lin <todoubaba@gmail.com>
2016-10-09 02:10:19 +0800

    Use CookieCsrfTokenRepository (#3)

--- tm-zuul/src/main/groovy/cn/edu/bnuz/bell/tm/zuul/WebSecurityConfiguration.groovy
+++ tm-zuul/src/main/groovy/cn/edu/bnuz/bell/tm/zuul/WebSecurityConfiguration.groovy
@@ -4,18 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.csrf.CsrfFilter
-import org.springframework.security.web.csrf.CsrfToken
-import org.springframework.security.web.csrf.CsrfTokenRepository
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository
-import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.web.util.WebUtils
-
-import javax.servlet.Filter
-import javax.servlet.FilterChain
-import javax.servlet.http.Cookie
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 /**
  * Web安全配置
@@ -34,34 +23,7 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
             .csrf()
                 .ignoringAntMatchers('/login', '/logout')
-                .csrfTokenRepository(csrfTokenRepository())
-                .and()
-            .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
-    }
-
-    private Filter csrfHeaderFilter() {
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName())
-                if (csrf != null) {
-                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN")
-                    String token = csrf.getToken()
-                    if (cookie == null || token != null && !token.equals(cookie.getValue())) {
-                        cookie = new Cookie("XSRF-TOKEN", token)
-                        cookie.setPath("/")
-                        response.addCookie(cookie)
-                    }
-                }
-                filterChain.doFilter(request, response)
-            }
-        };
-    }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
     }
 }
 
