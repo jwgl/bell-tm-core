1cec041737accc179c5c2ca5df1aa541e2cf1aae
Yang Lin <todoubaba@gmail.com>
2017-01-28 20:17:42 +0800

    Add student schedules (#19)

--- tm-plugin-common/grails-app/services/cn/edu/bnuz/bell/tm/common/operation/ScheduleService.groovy
+++ tm-plugin-common/grails-app/services/cn/edu/bnuz/bell/tm/common/operation/ScheduleService.groovy
@@ -6,27 +6,52 @@ import cn.edu.bnuz.bell.operation.TaskSchedule
 class ScheduleService {
     List getTeacherSchedules(String teacherId, Term term) {
         TaskSchedule.executeQuery '''
-select new Map(
-  schedule.id as scheduleId,
+select new map(
+  schedule.id as id,
   schedule.startWeek as startWeek,
   schedule.endWeek as endWeek,
   schedule.oddEven as oddEven,
   schedule.dayOfWeek as dayOfWeek,
   schedule.startSection as startSection,
   schedule.totalSection as totalSection,
+  course.name as course,
   courseItem.name as courseItem,
-  place.name as place,
-  course.name as course
+  place.name as place
 )
 from TaskSchedule schedule
 join schedule.task task
 join task.courseClass courseClass
 join courseClass.course course
-join schedule.teacher teacher
-left join schedule.place place
 left join task.courseItem courseItem
-where teacher.id = :teacherId
+left join schedule.place place
+where schedule.teacher.id = :teacherId
   and courseClass.term = :term
 ''', [teacherId: teacherId, term: term]
+    }
+
+    List getStudentSchedules(String studentId, Term term) {
+        TaskSchedule.executeQuery '''
+select new map(
+  schedule.id as id,
+  schedule.startWeek as startWeek,
+  schedule.endWeek as endWeek,
+  schedule.oddEven as oddEven,
+  schedule.dayOfWeek as dayOfWeek,
+  schedule.startSection as startSection,
+  schedule.totalSection as totalSection,
+  course.name as course,
+  courseItem.name as courseItem,
+  place.name as place
+)
+from TaskSchedule schedule
+join schedule.task task
+join task.courseClass courseClass
+join courseClass.course course
+join task.students taskStudent
+left join task.courseItem courseItem
+left join schedule.place place
+where taskStudent.student.id = :studentId
+  and courseClass.term = :term
+''', [studentId: studentId, term: term]
     }
 }
