a8cd4a792a986481b58b2866d6c3fb9fcd1d88d1
Yang Lin <todoubaba@gmail.com>
2017-02-17 20:47:24 +0800

    User profile

---
+++ tm-core-api/grails-app/services/cn/edu/bnuz/bell/profile/ProfileService.groovy
@@ -0,0 +1,18 @@
+package cn.edu.bnuz.bell.profile
+
+import cn.edu.bnuz.bell.security.User
+import grails.transaction.Transactional
+
+@Transactional
+class ProfileService {
+
+    def getUserInfo(String userId) {
+        User.get(userId)
+    }
+
+    def update(String userId, ProfileCommand cmd) {
+        User user = User.get(userId)
+        user.properties = cmd
+        user.save()
+    }
+}
