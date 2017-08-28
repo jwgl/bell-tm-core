989ce8c4b6a5eac353c7d2f1eadf68763c702707
Yang Lin <todoubaba@gmail.com>
2017-08-29 01:33:55 +0800

    New logout logic (#52)

--- tm-uaa/src/main/groovy/cn/edu/bnuz/bell/tm/uaa/AuthorizationServerConfiguration.groovy
+++ tm-uaa/src/main/groovy/cn/edu/bnuz/bell/tm/uaa/AuthorizationServerConfiguration.groovy
@@ -5,6 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import org.springframework.web.servlet.view.RedirectView
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpSession
 
 /**
  * 授权服务器配置
@@ -18,6 +26,29 @@ class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdap
     @Override
     void configure(ClientDetailsServiceConfigurer clients) {
         clients.withClientDetails(oAuthClientService)
+    }
+
+    /**
+     * 重定向前清除session，见https://github.com/spring-guides/tut-spring-security-and-angular-js/tree/master/oauth2-logout
+     */
+    @Override
+    void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.addInterceptor(new HandlerInterceptorAdapter() {
+            void postHandle(HttpServletRequest request,
+                            HttpServletResponse response, Object handler,
+                            ModelAndView modelAndView) throws Exception {
+                if (modelAndView != null && modelAndView.view instanceof RedirectView) {
+                    RedirectView redirect = (RedirectView) modelAndView.view
+                    String url = redirect.url
+                    if (url.contains("code=") || url.contains("error=")) {
+                        HttpSession session = request.getSession(false)
+                        if (session != null) {
+                            session.invalidate()
+                        }
+                    }
+                }
+            }
+        })
     }
 }
 
