445d670ea0e1dc892255ac449caec749e7c8c2da
Yang Lin <todoubaba@gmail.com>
2017-09-05 05:47:58 +0800

    Fix access token expired (#55)

--- tm-zuul/src/main/groovy/cn/edu/bnuz/bell/tm/zuul/WebSecurityConfiguration.groovy
+++ tm-zuul/src/main/groovy/cn/edu/bnuz/bell/tm/zuul/WebSecurityConfiguration.groovy
@@ -1,9 +1,14 @@
 package cn.edu.bnuz.bell.tm.zuul
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.oauth2.client.OAuth2ClientContext
+import org.springframework.security.oauth2.client.OAuth2RestOperations
+import org.springframework.security.oauth2.client.OAuth2RestTemplate
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 /**
@@ -17,7 +22,6 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     void configure(HttpSecurity http) throws Exception {
         http
-            .httpBasic().disable()
             .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
@@ -25,5 +29,9 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .ignoringAntMatchers('/login', '/logout')
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
     }
-}
 
+    @Bean
+    OAuth2RestOperations oAuth2RestOperations(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        new OAuth2RestTemplate(details, oauth2ClientContext)
+    }
+}
