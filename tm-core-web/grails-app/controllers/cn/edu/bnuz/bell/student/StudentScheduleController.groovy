073bc1a66e64170a0549b6f621bc8db76fd9deda
todoubaba <todoubaba@gmail.com>
2018-08-26 23:21:34 +0800

    Upgrade grails 3.3.8

--- tm-core-web/grails-app/controllers/cn/edu/bnuz/bell/student/StudentScheduleController.groovy
+++
@@ -1,6 +0,0 @@
-package cn.edu.bnuz.bell.student
-
-class StudentScheduleController {
-
-    def index() { }
-}
--- tm-core-web/grails-app/init/cn/edu/bnuz/bell/tm/zuul/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package cn.edu.bnuz.bell.tm.zuul
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
