3208a874bc19da82d170adfb2462c5725616789b
Yang Lin <todoubaba@gmail.com>
2019-08-16 21:01:45 +0800

    Use previous user info data

--- tm-zuul/grails-app/controllers/cn/edu/bnuz/bell/system/UserController.groovy
+++ tm-zuul/grails-app/controllers/cn/edu/bnuz/bell/system/UserController.groovy
@@ -6,6 +6,15 @@ class UserController {
     SecurityService securityService
 
     def index() {
-        renderJson([user: securityService.userDetails])
+        def userDetails = securityService.userDetails as Map
+        renderJson([
+                user: [
+                        id          : userDetails.id,
+                        name        : userDetails.name,
+                        type        : userDetails.userType,
+                        departmentId: userDetails.departmentId,
+                        phoneNumber : userDetails.longPhone,
+                ]
+        ])
     }
 }
